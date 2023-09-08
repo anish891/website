06cdef056f99cc3d092cc601db010bb977631721
Anish Tejwani <anishtejwani891@gmail.com>
2023-09-09 01:05:25 +0530

    pricing card removed and a few minor changes

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,7 +8,7 @@ const AboutContent = () => {
   return (
     <div className="about">
         <div className="left">
-            <h1>Who AM I?</h1>
+            <h1>Who am I?</h1>
             <p>I am Anish Tejwani who is always eager to take on new challenges and expand my skillset.</p>
             <Link to="/contact">
                 <button className="btn">Contact</button>
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImage2 from '../components/HeroImage2';
-import PricingCard from '../components/PricingCard';
+// import PricingCard from '../components/PricingCard';
 import Work from '../components/Work';
 
 const Project = () => {
@@ -11,7 +11,7 @@ const Project = () => {
       <Navbar />
       <HeroImage2 heading="PROJECTS." text="Some of my most recent work" />
       <Work />
-      <PricingCard />
+      {/* <PricingCard /> */}
       <Footer />
     </div>
   )
